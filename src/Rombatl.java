f316904170907a89137416a33d2299112e82701c
Dobany Norbert <ndobany@gmail.com>
2024-10-06 21:49:17 +0200

    comment update

--- src/Rombatl.java
+++ src/Rombatl.java
@@ -6,6 +6,17 @@
 * Date: 2024.10.03
 * Github: https://github.com/notdefinitelynot/
 * Licenc: GNU GPL
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details:
+* http://www.gnu.org/licenses/gpl.html
 
 A program egy rombusz területét számolja ki 
 a felhasználó által megadott két átlóból. 
